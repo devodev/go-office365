fe4d70eaa3c0ef4a20f38e76b394c35d4603945a
devodev <abalexandrebarone@gmail.com>
2020-03-26 18:40:52 -0400

    Add working code. needs refactoring but main idea is there

---
+++ go-office365/cmd/watch.go
@@ -0,0 +1,158 @@
+package cmd
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/devodev/go-graph/office365"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+func init() {
+	rootCmd.AddCommand(newCommandWatch())
+}
+
+func newCommandWatch() *cobra.Command {
+	var (
+		pubIdentifier string
+	)
+
+	cmd := &cobra.Command{
+		Use:   "watch [config]",
+		Short: "Fetch audit events at regular intervals.",
+		Args:  cobra.ExactArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			// command line args
+			confArg := args[0]
+
+			// load config file
+			watchConfig, err := loadConfig(confArg)
+			if err != nil {
+				fmt.Printf("error occured loading config file: %s\n", err)
+				return
+			}
+
+			// parse optional args
+			if pubIdentifier == "" {
+				pubIdentifier = config.Credentials.ClientID
+			}
+
+			// setup ticker using config interval
+			tickerDur := time.Duration(watchConfig.Global.TickerIntervalMinutes) * time.Second
+			ticker := time.NewTicker(tickerDur)
+
+			// setup signal handling
+			sigChan := getSigChan()
+
+			// create office365 client
+			client := office365.NewClientAuthenticated(&config.Credentials)
+
+			ctx, cancel := context.WithCancel(context.Background())
+
+			doneChan := make(chan bool)
+
+			// main loop
+			go func(o365Client *office365.Client, tc *time.Ticker, tDur time.Duration, done chan bool, exit <-chan os.Signal) {
+				for {
+					select {
+					case <-exit:
+						done <- true
+						break
+					case t := <-tc.C:
+
+						startTime := t.Add(-(tDur + time.Minute))
+						endTime := t
+
+						subscriptions, err := o365Client.Subscriptions.List(ctx, pubIdentifier)
+						if err != nil {
+							fmt.Printf("error getting subscriptions: %s\n", err)
+							break
+						}
+
+						for _, s := range subscriptions {
+
+							ct, err := office365.GetContentType(s.ContentType)
+							if err != nil {
+								fmt.Println(err)
+								continue
+							}
+
+							// retrieve content
+							content, err := o365Client.Subscriptions.Content(ctx, pubIdentifier, ct, startTime, endTime)
+							if err != nil {
+								fmt.Printf("error getting content: %s\n", err)
+								continue
+							}
+
+							// retrieve audits
+							var auditList []office365.AuditRecord
+							for _, c := range content {
+								audits, err := o365Client.Subscriptions.Audit(ctx, c.ContentID)
+								if err != nil {
+									fmt.Printf("error getting audits: %s\n", err)
+									continue
+								}
+								auditList = append(auditList, audits...)
+							}
+
+							// output
+							for _, a := range auditList {
+								auditStr, err := json.Marshal(a)
+								if err != nil {
+									fmt.Printf("error marshalling audit: %s\n", err)
+									continue
+								}
+								fmt.Println(string(auditStr))
+							}
+						}
+					}
+				}
+			}(client, ticker, tickerDur, doneChan, sigChan)
+
+			<-doneChan
+			cancel()
+		},
+	}
+	cmd.Flags().StringVar(&pubIdentifier, "identifier", "", "Publisher Identifier")
+
+	return cmd
+}
+
+// WatchConfig .
+type WatchConfig struct {
+	Global struct {
+		TickerIntervalMinutes int
+		PubIdentifier         string
+	}
+}
+
+func loadConfig(confPath string) (*WatchConfig, error) {
+	vip := viper.New()
+	vip.SetConfigFile(confPath)
+
+	if err := vip.ReadInConfig(); err != nil {
+		return nil, err
+	}
+
+	var config WatchConfig
+	if err := vip.UnmarshalExact(&config); err != nil {
+		return nil, err
+	}
+	return &config, nil
+}
+
+func getSigChan() chan os.Signal {
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan,
+		syscall.SIGHUP,
+		syscall.SIGINT,
+		syscall.SIGTERM,
+		syscall.SIGQUIT)
+	return sigChan
+}
