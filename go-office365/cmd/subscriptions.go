d11199a4ff288caf634c430e758072098eec9062
devodev <abalexandrebarone@gmail.com>
2020-03-31 15:44:32 -0400

    Added start/stop commands to cli

--- go-office365/cmd/subscriptions.go
+++ go-office365/cmd/subscriptions.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 
@@ -9,10 +10,12 @@ import (
 )
 
 func init() {
-	rootCmd.AddCommand(newCommandSubscriptions())
+	rootCmd.AddCommand(newCommandListSub())
+	rootCmd.AddCommand(newCommandStartSub())
+	rootCmd.AddCommand(newCommandStopSub())
 }
 
-func newCommandSubscriptions() *cobra.Command {
+func newCommandListSub() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "subscriptions",
 		Short: "List current subscriptions.",
@@ -25,13 +28,87 @@ func newCommandSubscriptions() *cobra.Command {
 				return
 			}
 			for _, u := range subscriptions {
-				userData, err := json.Marshal(u)
+				payload, err := json.Marshal(u)
 				if err != nil {
 					logger.Printf("error marshalling subscriptions: %s\n", err)
 					continue
 				}
-				WriteOut(string(userData))
+				var out bytes.Buffer
+				json.Indent(&out, payload, "", "\t")
+				WriteOut(out.String())
 			}
+		},
+	}
+	return cmd
+}
+
+func newCommandStartSub() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "start-sub [content-type]",
+		Short: "Start a subscription for the provided Content Type.",
+		Args:  cobra.ExactArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			// command line args
+			ctArg := args[0]
+
+			// validate args
+			if !office365.ContentTypeValid(ctArg) {
+				logger.Println("ContentType invalid")
+				return
+			}
+			ct, err := office365.GetContentType(ctArg)
+			if err != nil {
+				logger.Println(err)
+				return
+			}
+
+			client := office365.NewClientAuthenticated(&config.Credentials, config.Global.Identifier)
+			subscription, err := client.Subscription.Start(context.Background(), ct, nil)
+			if err != nil {
+				logger.Printf("error getting subscriptions: %s\n", err)
+				return
+			}
+			payload, err := json.Marshal(subscription)
+			if err != nil {
+				logger.Printf("error marshalling subscription: %s\n", err)
+				return
+			}
+			var out bytes.Buffer
+			json.Indent(&out, payload, "", "\t")
+			WriteOut(out.String())
+
+			WriteOut("subscription successfully started")
+		},
+	}
+	return cmd
+}
+func newCommandStopSub() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "start-sub [content-type]",
+		Short: "Stop a subscription for the provided Content Type.",
+		Args:  cobra.ExactArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			// command line args
+			ctArg := args[0]
+
+			// validate args
+			if !office365.ContentTypeValid(ctArg) {
+				logger.Println("ContentType invalid")
+				return
+			}
+			ct, err := office365.GetContentType(ctArg)
+			if err != nil {
+				logger.Println(err)
+				return
+			}
+
+			client := office365.NewClientAuthenticated(&config.Credentials, config.Global.Identifier)
+			if err := client.Subscription.Stop(context.Background(), ct); err != nil {
+				logger.Printf("error getting subscriptions: %s\n", err)
+				return
+			}
+
+			WriteOut("subscription successfully stopped")
 		},
 	}
 	return cmd
