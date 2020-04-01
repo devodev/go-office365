192bea3a85c892d8b32548adcdf2fee9f3e30544
devodev <abalexandrebarone@gmail.com>
2020-04-01 18:05:32 -0400

    Implement output target selection. This is still quite ugly and we are not handling all edge cases nor have a way to setup tls. Lets work with it a bit and see.

--- go-office365/cmd/watch.go
+++ go-office365/cmd/watch.go
@@ -3,9 +3,12 @@ package cmd
 import (
 	"context"
 	"fmt"
+	"io"
+	"net"
 	"os"
 	"os/signal"
 	"path/filepath"
+	"strings"
 	"syscall"
 
 	"github.com/devodev/go-office365/office365"
@@ -30,8 +33,7 @@ func newCommandWatch() *cobra.Command {
 		Short: "Fetch audit events at regular intervals.",
 		Args:  cobra.NoArgs,
 		Run: func(cmd *cobra.Command, args []string) {
-			client := office365.NewClientAuthenticated(&config.Credentials, config.Global.Identifier)
-
+			// create cancelling context based on signals
 			ctx, cancel := context.WithCancel(context.Background())
 			go func() {
 				sigChan := getSigChan()
@@ -44,26 +46,79 @@ func newCommandWatch() *cobra.Command {
 				}
 			}()
 
-			watcherConf := office365.SubscriptionWatcherConfig{
-				LookBehindMinutes:     lookBehindMinutes,
-				TickerIntervalSeconds: intervalSeconds,
-			}
-
+			// Create state instance
 			state := office365.NewGOBState()
 			if statefile != "" {
 				statefileAbs, writeStateDefer, err := setupStatefile(state, statefile)
 				if err != nil {
 					logger.Println(err)
+					// ? TODO: Nested exit path
+					return
 				}
 				defer writeStateDefer()
 				logger.Printf("using statefile: %q\n", statefileAbs)
 			}
 
+			// Select output target
+			// TODO: move into setup function
+			filePrefix := "file://"
+			udpPrefix := "udp://"
+			tcpPrefix := "tcp://"
+
+			var writer io.Writer
+			switch {
+			default:
+				logger.Println("output invalid")
+				return
+			case output == "":
+				writer = defaultOutput
+			case strings.HasPrefix(output, filePrefix):
+				path := output[len(filePrefix):len(output)]
+				f, close, err := openOutputfile(path)
+				if err != nil {
+					logger.Printf("failed to create/open file: %s\n", err)
+					// ? TODO: Nested exit path
+					return
+				}
+				defer close()
+				logger.Printf("using file output: %q\n", path)
+				writer = f
+			case strings.HasPrefix(output, udpPrefix):
+				path := output[len(udpPrefix):len(output)]
+				var d net.Dialer
+				conn, err := d.DialContext(ctx, "udp", path)
+				if err != nil {
+					logger.Printf("Failed to dial udp: %s\n", err)
+					// ? TODO: Nested exit path
+					return
+				}
+				defer conn.Close()
+			case strings.HasPrefix(output, tcpPrefix):
+				path := output[len(tcpPrefix):len(output)]
+				var d net.Dialer
+				conn, err := d.DialContext(ctx, "tcp", path)
+				if err != nil {
+					logger.Printf("Failed to dial tcp: %s\n", err)
+					// ? TODO: Nested exit path
+					return
+				}
+				defer conn.Close()
+			}
+
+			// Select resource handler
 			var handler office365.ResourceHandler
 			if humanReadable {
-				handler = office365.NewHumanReadableHandler(defaultOutput)
+				handler = office365.NewHumanReadableHandler(writer)
 			} else {
-				handler = office365.NewJSONHandler(defaultOutput, logger)
+				handler = office365.NewJSONHandler(writer, logger)
+			}
+
+			// Create client and launch watcher
+			client := office365.NewClientAuthenticated(&config.Credentials, config.Global.Identifier)
+
+			watcherConf := office365.SubscriptionWatcherConfig{
+				LookBehindMinutes:     lookBehindMinutes,
+				TickerIntervalSeconds: intervalSeconds,
 			}
 			if err := client.Subscription.Watch(ctx, watcherConf, state, handler); err != nil {
 				logger.Printf("error occured calling watch: %s\n", err)
@@ -89,14 +144,21 @@ func getSigChan() chan os.Signal {
 	return sigChan
 }
 
+func openOutputfile(fpath string) (*os.File, func() error, error) {
+	f, err := os.OpenFile(fpath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0755)
+	if err != nil {
+		return nil, nil, err
+	}
+	return f, f.Close, nil
+}
+
 func setupStatefile(state *office365.GOBState, fpath string) (string, func() error, error) {
 	statefile, err := filepath.Abs(fpath)
 	if err != nil {
 		return "", nil, fmt.Errorf("could not get absolute filepath for provided statefile: %s", err)
 	}
 
-	err = readState(state, statefile)
-	if err != nil {
+	if err := readState(state, statefile); err != nil {
 		return "", nil, fmt.Errorf("error occured setuping statefile: %s", err)
 	}
 
@@ -104,7 +166,6 @@ func setupStatefile(state *office365.GOBState, fpath string) (string, func() err
 		return writeState(state, statefile)
 	}
 	return statefile, deferred, nil
-
 }
 
 func openStatefile(fpath string) (*os.File, func() error, error) {
